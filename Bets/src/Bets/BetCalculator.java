70fbac0fdbd8f5e470a22a1e936e69bb8ae0a322
ItsmeMarcipan07 <martipan07@abv.bg>
2025-01-16 18:57:51 +0200

    Delete Bets/src/Bets/BetCalculator.java

--- Bets/src/Bets/BetCalculator.java
+++
@@ -1,8 +0,0 @@
-package Bets;
-
-public class BetCalculator {
-    public static double calculatePayout(double betAmount, double odds, boolean isWon) {
-        return isWon ? betAmount * odds : 0;
-    }
-}
-
